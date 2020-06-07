6b73b2563c5abcb514f05082728d60fe40bb14ab
Hussain Saifuddin <56501305+hussain385@users.noreply.github.com>
2020-06-07 22:35:44 +0500

    trying horeku

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+use rocket::request::Form;
+use rocket::response::content::Html;
+#[derive(FromForm)]
+struct Number{
+    number : u32,
+}
 
-#[get("/<random_num>")]
-fn assigment(random_num: u16) -> & String {
-    format!("{}", random_num + 50)
+#[post("/num", data = "<number>")]
+fn addition(number : Form<Number>) -> Html<String> {
+   let h1=format!("<p>{}</p>", number.number+50);
+    Html(h1)
+}
+#[get("/num")]
+fn input_number()-> Html<String>{
+    let h1=format!("
+        <form action='/num' method='post'>
+            <input type='number' placeholder='Place Your Number'
+                name='number' id='number'>
+            <input type='Submit'>
+        </form>
+    ");
+    Html(h1)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![assigment]).launch();
+    rocket::ignite().mount("/", routes![input_number,addition]).launch();
 }
